1dd84a58b7eab00e2bc00b83edb43bb53b1ad3ab
ameykpatel <ameykpatel@gmail.com>
2019-08-02 17:31:08 +0530

    Add files via upload

---
+++ src/ui/shared/content_box.js
@@ -0,0 +1,90 @@
+export const sidebar1 = [
+    {
+        id: 'Delay',
+        typeName: 'Delay',
+        image: '/assets/images/Delay.PNG',
+        // content: "Pan",
+        // additional: "1",
+        // icon: "https://picsum.photos/id/10/50/50",
+        lastIndex: 1
+          },
+          {
+            id: 'SignalGen',
+            typeName: 'SignalGen',
+        image: '/assets/images/SignalGen.PNG',
+        lastIndex:1
+            },
+      {
+        id: 'Speaker',
+        typeName: 'Speaker',
+        image: '/assets/images/Speaker.PNG',
+        // content: "Transposer",
+        // additional: "2",
+        // icon: "https://picsum.photos/id/11/50/50",
+        lastIndex:1
+          },
+      {
+          id: 'DirectInput',
+          typeName: 'DirectInput',
+        image: '/assets/images/DirectInput.PNG',
+        lastIndex:1
+              },
+      {
+        id: 'Mixer',
+        typeName: 'Mixer',
+        image: '/assets/images/Mixer.PNG',
+        lastIndex:1
+        },
+    {
+        id: 'Pan',
+        typeName: 'Pan',
+        image: '/assets/images/Pan.PNG',
+        lastIndex:1
+        }
+
+
+];
+
+export const sidebar2 = [
+    // {
+    //     id: 'delay',
+    //     // content: "Delay",
+    //     // additional: "3",
+    //     // icon: "https://picsum.photos/id/12/50/50",
+    //     lastIndex:1
+        //   }
+
+    {
+        id: 'Pitch',
+        typeName: 'Pitch',
+        image: '/assets/images/Pitch.PNG',
+        lastIndex:1
+        },
+    {
+        id: 'Routing',
+        typeName: 'Routing',
+        image: '/assets/images/Routing.PNG',
+        lastIndex:1
+        },
+  
+    
+    {
+        id: 'Transposer',
+        image: '/assets/images/Transposer.PNG',
+        typeName: 'Transposer',
+        lastIndex:1
+        },
+    {
+        id: 'VSTHost',
+        typeName: 'VSTHost',
+        image: '/assets/images/VST.PNG',
+        lastIndex:1
+        }
+];
+
+export const major1 = [
+];
+
+export const major2 = [
+    
+];
